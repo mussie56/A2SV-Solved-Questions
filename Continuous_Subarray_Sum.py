128a2ea0fd6295d37cad4363d70b2f50d28b5786
Mussie Ferede <mossferede@gmail.com>
2026-03-03 19:07:19 +0300

    Create Continuous_Subarray_Sum.py

---
+++ Continuous_Subarray_Sum.py
@@ -0,0 +1,13 @@
+class Solution:
+    def checkSubarraySum(self, nums: List[int], k: int) -> bool:
+        pref = {0:-1}
+        summ = 0
+        for i in range(len(nums)):
+            summ+=nums[i]
+            if summ%k in pref:
+                if i-pref[summ%k] >= 2:
+                    return True
+            else:
+                pref[summ%k] = i
+
+        return False
